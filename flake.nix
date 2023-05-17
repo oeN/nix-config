43a18c40d43cc8572d66a7b8faa5409a919399fd
Diomede T <diomede.tripicchio@gmail.com>
2023-05-17 11:47:14 +0200

    remove x86_64 overlays

--- flake.nix
+++ flake.nix
@@ -22,16 +22,6 @@
     # Configuration for `nixpkgs`
     nixpkgsConfig = {
       config = { allowUnfree = true; };
-      overlays = attrValues self.overlays ++ singleton (
-        # Sub in x86 version of packages that don't build on Apple Silicon yet
-        final: prev: (optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
-          inherit (final.pkgs-x86)
-            idris2
-            nix-index
-            niv
-            purescript;
-        })
-      );
     }; 
   in
   {
@@ -55,17 +45,5 @@
         ];
       };
     };
-
-    overlays = {
-        # Overlay useful on Macs with Apple Silicon
-        # TODO: understand how this works
-        apple-silicon = final: prev: optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
-          # Add access to x86 packages system is running Apple Silicon
-          pkgs-x86 = import inputs.nixpkgs {
-            system = "x86_64-darwin";
-            inherit (nixpkgsConfig) config;
-          };
-        }; 
-      };
  };
 }
